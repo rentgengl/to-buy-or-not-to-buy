2492689d080593b8ed60e0892affe2dea939862b
Den <denacid@mail.ru>
2019-01-13 17:00:31 +0300

    Добавил суффикс для теста

--- app/src/main/java/com/world/jteam/bonb/MainActivity.java
+++ app/src/main/java/com/world/jteam/bonb/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.LinkedHashMap;
 
 public class MainActivity extends AppCompatActivity {
     //Леонов
-    //Шакун
+    //Шакун2
     private final AppCompatActivity mThis=this;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
