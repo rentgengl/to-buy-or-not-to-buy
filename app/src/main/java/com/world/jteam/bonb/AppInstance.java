bc9a1a58dce814aa0fb8c8b6029151bfa622f9cd
rentgengl <leongl@ya.ru>
2019-02-08 21:22:52 +0300

    Форма ввода коммента

--- app/src/main/java/com/world/jteam/bonb/AppInstance.java
+++ app/src/main/java/com/world/jteam/bonb/AppInstance.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 public class AppInstance extends Application {
     private static Context sContext;
     private static boolean sFirstStart;
-    private ModelUser user;
+    private static ModelUser sUser;
 
     private static boolean sAutoGeoPosition = true;
     private static int sRadiusArea = Constants.DEFAULT_RADIUS_AREA;
@@ -75,11 +75,11 @@ public class AppInstance extends Application {
 
     //Мультидекс - необходимый фикс для запуска приложения
     public static ModelUser getUser(){
-        return null;
+        return AppInstance.sUser;
     }
 
     public static void setUser(ModelUser user){
-
+        AppInstance.sUser = user;
     }
 
     @Override
