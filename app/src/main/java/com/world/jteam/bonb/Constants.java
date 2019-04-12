1be2a89c359e57f735377d6c0d712ef04e8c0694
rentgengl <leongl@ya.ru>
2019-04-12 21:08:12 +0300

    Переключил на новый серв

--- app/src/main/java/com/world/jteam/bonb/Constants.java
+++ app/src/main/java/com/world/jteam/bonb/Constants.java
@@ -11,30 +11,21 @@ public class Constants {
 
 
     //HTTP
-    public static final String HTTP_SERVER = "http://tst.tradecode.ru/";
+    public static final String HTTP_SERVER = "https://goodno.ru/services/";
 
-    public static final String SERVICE_GET_PRODUCT = HTTP_SERVER + "GetProductByEAN.php?";
-    public static final String SERVICE_GET_PRODUCTFULL_ID = HTTP_SERVER + "GetProductFullById.php?";
-    public static final String SERVICE_GET_PRODUCTFULL_EAN = HTTP_SERVER + "GetProductFullByEAN.php?";
-    public static final String SERVICE_GET_PRODUCT_LIST_BY_GROUP = HTTP_SERVER + "GetProductListByGroup.php?";
-    public static final String SERVICE_GET_PRODUCT_GROU_LIST_BY_NAME = HTTP_SERVER + "GetProductGroupListByName.php?";
     public static final String SERVICE_POST_NEW_PRODUCT = HTTP_SERVER + "NewProduct.php";
 
+    //Picasso работает только с http, а мне и не жалко
     public static final String SERVER_PATH_IMAGE = "img/";
-    public static final String SERVICE_GET_IMAGE = HTTP_SERVER + SERVER_PATH_IMAGE;
+    public static final String SERVICE_GET_IMAGE = "http://goodno.ru/" + SERVER_PATH_IMAGE;
 
     public static final String SERVER_PATH_GROUPS_LOGO = "groups_logo/";
-    public static final String SERVICE_GET_GROUPS_LOGO = HTTP_SERVER + SERVER_PATH_GROUPS_LOGO;
+    public static final String SERVICE_GET_GROUPS_LOGO = "http://goodno.ru/" + SERVER_PATH_GROUPS_LOGO;
 
 
     public static Integer DEFAULT_PER_PAGE = 15; //Количество элементов считываемых с сервера
     public static int MAX_PRODUCT_LIST_ITEMS = 1000; // Максимальное количество которое можно вывести в список
     public static int MAX_PRODUCT_LIST_FLING_Y = 5000; //максимальная скорость броска списка
-
-    public static final String SEARCH_METHOD_BY_ID = "SEARCH_METHOD_BY_ID";
-    public static final String SEARCH_METHOD_BY_EAN = "SEARCH_METHOD_BY_EAN";
-    public static final String SEARCH_METHOD_BY_NAME = "SEARCH_METHOD_BY_NAME";
-    public static final String SEARCH_METHOD_BY_GROUP = "SEARCH_METHOD_BY_GROUP";
 
     public static final int DECODE_IMAGE_SIZE_SMALL=30;
     public static final int DECODE_IMAGE_SIZE_MEDIUM=100;
