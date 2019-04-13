eada1be9da6a9b75d409722146c1ab3da11366a3
Den <denacid@mail.ru>
2019-04-13 16:37:22 +0300

    Вычистил мусор

--- app/src/main/java/com/world/jteam/bonb/activity/MainActivity.java
+++ app/src/main/java/com/world/jteam/bonb/activity/MainActivity.java
@@ -23,9 +23,11 @@ import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +39,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.world.jteam.bonb.AppInstance;
-import com.world.jteam.bonb.MarketPageFragment;
 import com.world.jteam.bonb.geo.GeoManager;
 import com.world.jteam.bonb.ldrawer.ActionBarDrawerToggle;
 import com.world.jteam.bonb.ldrawer.DrawerArrowDrawable;
@@ -541,7 +542,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    //Адаптер страниц
+    //Страницы
+    public static class MarketPageFragment extends Fragment {
+        static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+        int pageNumber;
+        public View page1_List = null;
+        public View page2_Info = null;
+
+        static MarketPageFragment newInstance(int page,View page1, View page2) {
+            MarketPageFragment pageFragment = new MarketPageFragment();
+
+            pageFragment.page1_List = page1;
+            pageFragment.page2_Info = page2;
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+            pageFragment.setArguments(arguments);
+            return pageFragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            switch (pageNumber) {
+                case (1):
+                    if (page2_Info == null) {
+                        page2_Info = inflater.inflate(R.layout.fragment_market_contacts, null);
+                    }
+                    return page2_Info;
+                case (2):
+
+                    if (page1_List == null) {
+                        page1_List = inflater.inflate(R.layout.fragment_market_product_list, null);
+                    }
+                    return page1_List;
+                default:
+                    if (page1_List == null) {
+                        page1_List = inflater.inflate(R.layout.fragment_market_product_list, null);
+                    }
+                    return page1_List;
+            }
+
+        }
+    }
+
     private class MarketFragmentPagerAdapter extends FragmentPagerAdapter {
 
         public View page1;
